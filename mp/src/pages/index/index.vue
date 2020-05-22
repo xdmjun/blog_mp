eaf67c07a06e36f05ac17fcb42f9bf556b580095
xg <490599161@qq.com>
2020-05-22 16:11:22 +0800

    index style

--- mp/src/pages/index/index.vue
+++ mp/src/pages/index/index.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="main">
-    <i-cell-group>
-      <i-cell
-        v-for="blog in blogs"
-        :key="index"
-        :title="blog.title"
-        @click="toDetail(blog.file_name)"
-      ></i-cell>
-    </i-cell-group>
+    <swiper
+      class="banner"
+      indicator-dots="true"
+      autoplay="true"
+      interval="5000"
+      duration="300"
+      circular="true"
+    >
+      <div v-for="(item,i) in bannerList" :key="i">
+        <swiper-item>
+          <image :src="item" lazy-load="true" class="slide-image" mode="aspectFit" />
+        </swiper-item>
+      </div>
+    </swiper>
+    <div class="blogs-box">
+      <div class="title">最新博客</div>
+      <i-cell-group>
+        <i-cell
+          v-for="(blog,index) in blogs"
+          :key="index"
+          :title="blog.title"
+          is-link
+          @click="toDetail(blog.file_name)"
+        >
+          <i-icon v-if="blog.recommend" color="red" type="praise_fill" slot="icon" />
+        </i-cell>
+      </i-cell-group>
+    </div>
   </div>
 </template>
 
@@ -16,24 +36,27 @@
 export default {
   data() {
     return {
-      blogs: []
+      blogs: [],
+      bannerList: [
+        'http://xuedingmiao.com/images/bm1.png',
+        'http://xuedingmiao.com/images/dau-value.png',
+        'http://xuedingmiao.com/images/gitcmd.png',
+      ],
     }
   },
   methods: {
     toDetail(name) {
       mpvue.navigateTo({
-        url: '/pages/blog/main?name=' + name
+        url: '/pages/blog/main?name=' + name,
       })
     },
   },
-  onShareAppMessage() {
-
-  },
+  onShareAppMessage() {},
   onLoad() {
     wx.cloud
       .callFunction({
         name: 'blogs',
-        data: {}
+        data: {},
       })
       .then(res => {
         let rs = JSON.parse(res.result)
@@ -43,8 +66,7 @@ export default {
   mounted() {
     wx.showShareMenu()
   },
-  onShow() {
-  },
+  onShow() {},
 }
 </script>
 
@@ -53,6 +75,20 @@ page {
   background: #fff;
 }
 .main {
-  padding: 0 5px;
+  .banner {
+    width: 100%;
+    margin-bottom: 5px;
+    .slide-image {
+      width: 100%;
+    }
+  }
+  .blogs-box {
+    padding: 0 5px;
+    .title {
+      font-size: 16px;
+      border-bottom: 1px solid #efefef;
+      padding: 5px 15px;
+    }
+  }
 }
 </style>
