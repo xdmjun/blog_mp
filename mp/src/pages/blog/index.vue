28d25ceef0b2965b0b88a09bd81237dcbd4dcaa0
xg <490599161@qq.com>
2020-05-24 19:28:13 +0800

    外链视频提示

--- mp/src/pages/blog/index.vue
+++ mp/src/pages/blog/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="main">
+    <i-alert v-if="showVideoTip" type="warning">文章包含外链视频，个人小程序不支持播放。如需观看，请移步Web端查看</i-alert>
     <div class="title">{{title}}</div>
     <div class="content">
       <wemark :md="content" link highlight type="wemark"></wemark>
@@ -15,6 +16,7 @@ export default {
       title: '',
       name: '',
       content: '',
+      showVideoTip: false,
     }
   },
   methods: {},
@@ -30,6 +32,9 @@ export default {
         this.title = rs.data.title
         mpvue.setNavigationBarTitle({ title: this.title })
         this.content = rs.data.content.replace(/[\\]/g, '')
+        if (this.content.indexOf('iframe') !== -1) {
+          this.showVideoTip = true
+        }
       })
   },
   onShow() {},
